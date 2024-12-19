0ccef574c4b7f35c4f0af62453b152e4d4148670
StevenXuXu <xsjgreat@163.com>
2024-12-19 16:17:05 +0800

    基于session实现短信登录注册

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,25 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -14,7 +29,59 @@ import org.springframework.stereotype.Service;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        if(RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式错误");
+        }
+
+        String code = RandomUtil.randomNumbers(6);
+
+        session.setAttribute("code", code);
+
+        log.debug("发送短信验证发成功，验证码{}", code);
+
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        if(RegexUtils.isPhoneInvalid(loginForm.getPhone())) {
+            return Result.fail("手机号格式错误");
+        }
+
+        if(StrUtil.isBlank(loginForm.getPassword()) && StrUtil.isBlank(loginForm.getCode())) {
+            return Result.fail("验证码和密码为空");
+        }
+
+        String code = session.getAttribute("code").toString();
+
+        if(StrUtil.isNotBlank(code) && StrUtil.isNotBlank(loginForm.getCode()) && !code.equals(loginForm.getCode())) {
+            return Result.fail("验证码错误");
+        }
+
+        // 验证密码...
+
+        User user = lambdaQuery().eq(User::getPhone, loginForm.getPhone()).one();
+
+        if(user == null) {
+            user = createUserWithPhone(loginForm.getPhone());
+        }
+
+        session.setAttribute("user", BeanUtil.copyProperties(user, UserDTO.class));
+
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        save(user);
+        return user;
+    }
 }
